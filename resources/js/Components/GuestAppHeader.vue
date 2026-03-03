1a9e4076e6c0821bd559838b5d15fa0fceb3a61a
Gabriel Moreira <gmdoas.dev@gmail.com>
2026-03-03 13:37:47 -0300

    tela de login responsiva

---
+++ resources/js/Components/GuestAppHeader.vue
@@ -0,0 +1,23 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue'
+import { Link } from '@inertiajs/vue3'
+</script>
+
+<template>
+    <header class="bg-primary">
+        <div class="flex h-16 items-center justify-between px-8">
+            <div href="/" class="flex items-center space-x-2">
+                <span class="text-white font-semibold text-lg">
+                    e-fomento
+                </span>
+            </div>
+            <span
+                @click="$inertia.visit(route('register'))"
+                class="cursor-pointer rounded-md px-4 py-2 text-sm font-medium text-black bg-white transition hover:bg-gray-500"
+            >
+                Solicitar acesso
+            </span>
+            
+        </div>
+    </header>
+</template>
