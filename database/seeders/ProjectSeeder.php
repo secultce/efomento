8f7fd1380985b9c7d9d4dd2fcf8fcbf0989da336
Junior-Shyko <franciscoanto@gmail.com>
2026-03-03 15:18:00 -0300

    :seedling: #19 add seed com os editais via json

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,11 +4,22 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Project;
-
 class ProjectSeeder extends Seeder
 {
     public function run(): void
     {
-        Project::factory()->count(20)->create();
+        $json = file_get_contents(database_path('seeders/data/editais_project_pnab.json'));
+        $editais = json_decode($json, associative: true);
+
+        foreach ($editais as $edital) {
+            Project::updateOrCreate(
+                ['nup' => 'NUP-' . $edital['id']],
+                [
+                    'external_id' => $edital['id'],
+                    'name'        => $edital['nome'],
+                    'project_url' => (string) $edital['id_projeto'],
+                ]
+            );
+        }
     }
 }
