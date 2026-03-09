75dffe3541fea83005a64f0acb38d8126bb8a7dc
jgaguiarm <jaimgaguiar@gmail.com>
2026-03-09 11:58:24 -0300

    Implementa os testes para a feature login

---
+++ cypress/e2e/efomento/auth/login.cy.js
@@ -0,0 +1,72 @@
+// import users from '..'
+
+describe('Login', () => {
+
+  let data;
+
+  before(() => {
+    cy.fixture('users').then((tData) => {
+      data = tData;
+    })
+  })
+
+  it('Login com sucesso', () => {
+    cy.visit('/login')
+    cy.get('#email')
+      .type(data.valid_email)
+    cy.get('#password')
+      .type(data.password)
+    cy.get('button').contains('Entrar')
+      .should('be.visible')
+      .click()
+    cy.url('/editais')
+      .should('be.have')
+    cy.get('p')
+      .contains('Bem-vindo ao seu espaço,')
+      .should('be.visible')
+
+  })
+
+  it('Login com senha incorreta',() => {
+    cy.visit('/login')
+    cy.get('#email')
+      .type(data.valid_email)
+    cy.get('#password')
+      .type(data.incorret_password)
+    cy.get('button').contains('Entrar')
+      .should('be.visible')
+      .click()
+    cy.get(':nth-child(1) > .mt-2 > .text-sm')
+      .contains('These credentials do not match our records.')
+      .should('be.visible')
+  })
+
+  it('Login com email inválido', () => {
+    cy.visit('/login')
+    cy.get('#email')
+      .type(data.invalid_email)
+      .should('have.prop', 'validity')
+      .and('include', { valid: false})
+    cy.get('#password')
+      .type(data.password)
+    cy.get('button').contains('Entrar')
+      .should('be.visible')
+      .click()
+  })
+
+  it('Valida que acesso à rota /editais deslogado redireciona para /login', () => {
+    cy.visit('/editais')
+    cy.url()
+      .should('be.equal', `${Cypress.config("baseUrl")}/login`)  
+  })
+
+  it('Valida que ao clicar no botão de logout redireciona para /login', () => {
+    cy.login('alverta.satterfield@example.com','password');
+    cy.get('span>.v-avatar')
+      .click()
+    cy.contains('Sair')
+      .click()
+    cy.url()
+      .should('be.equal', `${Cypress.config("baseUrl")}/`)
+  })
+})
