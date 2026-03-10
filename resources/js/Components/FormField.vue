c77edbc6fa5e7bd110b9ac0ec956787043692d46
Gabriel Moreira <gmdoas.dev@gmail.com>
2026-03-10 11:13:12 -0300

    ajustando espaçamento

--- resources/js/Components/FormField.vue
+++ resources/js/Components/FormField.vue
@@ -12,14 +12,14 @@ const slots = useSlots()
 </script>
 
 <template>
-    <label class="text-xs font-weight-bold mb-2 d-block">
+    <label class="text-xs font-weight-bold d-block">
         {{ label }}
         <span v-if="required" class="text-red-500">*</span>
     </label>
 
     <component v-for="vnode in slots.default?.()" :is="cloneVNode(vnode, { required, clearable })" />
 
-    <p v-if="error" class="text-red-500 text-xs mt-1">
+    <p v-if="error" class="text-red-500 text-xs">
         {{ error }}
     </p>
 </template>
