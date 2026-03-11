6f825394901941338978f9dc373fe8a7ef727a81
Gabriel Moreira <gmdoas.dev@gmail.com>
2026-03-11 15:02:23 -0300

    organizando requests

--- app/Http/Requests/Agent/AgentStoreRequest.php
+++ app/Http/Requests/Agent/AgentStoreRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Agent;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class AgentUpdateRequest extends FormRequest
+class AgentStoreRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
--- app/Http/Requests/Agent/AgentUpdateRequest.php
+++ app/Http/Requests/Agent/AgentUpdateRequest.php
@@ -1,10 +1,9 @@
 <?php
 
-namespace App\Http\Requests;
-
+namespace App\Http\Requests\Agent;
 use Illuminate\Foundation\Http\FormRequest;
 
-class AgentStoreRequest extends FormRequest
+class AgentUpdateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
