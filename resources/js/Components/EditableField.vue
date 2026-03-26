2e6add4c2b0587f117ee9a86ded8451702bcfded
Gabriel Moreira <gmdoas.dev@gmail.com>
2026-03-26 14:40:31 -0300

    movendo logica para composables

--- resources/js/Components/EditableField.vue
+++ resources/js/Components/EditableField.vue
@@ -1,5 +1,10 @@
 <script setup>
 import { ref, watch } from 'vue'
+import { useDate } from '@/Composables/useDate'
+import { useMask } from '@/Composables/useMask'
+
+const { formatDate, normalizeDate } = useDate()
+const { applyMask } = useMask()
 
 const props = defineProps({
     modelValue: [String, Number, Array, Object, Date],
@@ -26,55 +31,6 @@ const emit = defineEmits(['update:modelValue'])
 
 const editing = ref(false)
 const localValue = ref(props.modelValue)
-
-const formatDate = (value) => {
-    if (!value) return '-'
-
-    if (typeof value === 'string' && value.includes('-')) {
-        const [year, month, day] = value.slice(0, 10).split('-')
-        return `${day}/${month}/${year}`
-    }
-
-    const date = new Date(value)
-
-    return date.toLocaleDateString('pt-BR')
-}
-
-const normalizeDate = (value) => {
-    if (!value) return null
-
-    if (typeof value === 'string' && value.includes('-')) {
-        return value.slice(0, 10)
-    }
-
-    const date = new Date(value)
-
-    const year = date.getFullYear()
-    const month = String(date.getMonth() + 1).padStart(2, '0')
-    const day = String(date.getDate()).padStart(2, '0')
-
-    return `${year}-${month}-${day}`
-}
-
-function applyMask(value) {
-    if (!props.mask) return value
-    if (!value) return ''
-    const digits = value.replace(/\D/g, '')
-    let result = ''
-    let digitIndex = 0
-
-    for (let i = 0; i < props.mask.length; i++) {
-        if (props.mask[i] === '#') {
-            if (digits[digitIndex] === undefined) break
-            result += digits[digitIndex]
-            digitIndex++
-        } else {
-            result += props.mask[i]
-        }
-    }
-
-    return result
-}
 
 watch(() => props.modelValue, (val) => {
     if (props.type === 'date' && val) {
@@ -104,7 +60,7 @@ const stopEdit = () => {
 
             <!-- TEXT / NUMBER -->
             <v-text-field v-if="type === 'text' || type === 'number' || type === 'email'"
-                :model-value="props.mask ? applyMask(localValue) : localValue" @update:modelValue="val => {
+                :model-value="props.mask ? applyMask(localValue, props.mask) : localValue" @update:modelValue="val => {
                     localValue = props.mask ? val.replace(/\D/g, '') : val
                 }" :type="type" density="compact" autofocus hide-details :error-messages="error" @blur="stopEdit"
                 @keyup.enter="stopEdit" />
@@ -150,7 +106,7 @@ const stopEdit = () => {
                 <template v-else>
                     {{ modelValue ?
                         props.mask
-                            ? applyMask(String(modelValue))
+                            ? applyMask(String(modelValue), props.mask)
                             : modelValue
                         : '-'
                     }}
