327ebb555b735ea989d72f5c02cf95cf54a8132a
jgaguiarm <jaimgaguiar@gmail.com>
2026-03-30 15:30:48 -0300

    Implementa padrão Page Object para testes de login

---
+++ cypress/pages/auth/index.js
@@ -0,0 +1,68 @@
+import { elements as el} from "./elements"
+
+class Login{
+    
+    acessarPaginaDeLogin() {
+        cy.visit('/login')
+    }
+
+    acessarPaginaDeEditais() {
+        cy.visit('/editais')  
+    }
+
+    loginComSucesso(email, password, name) {
+        cy.get(el.inputEmail)
+          .type(email)
+        cy.get(el.inputPassword)
+          .type(password)
+        cy.get(el.loginButton)
+          .should('be.visible')
+          .click()
+        cy.url()
+          .should('be.equal', `${Cypress.config("baseUrl")}/editais`)  
+        cy.contains(el.welcomeMessage + name)
+          .should('be.visible')
+    }
+
+    loginComSenhaIncorreta(email, password) {
+        cy.get(el.inputEmail)
+          .type(email)
+        cy.get(el.inputPassword)
+          .type(password)            
+        cy.get(el.loginButton)
+          .should('be.visible')
+          .click()
+        cy.get('p')
+          .contains(el.passwordErrorMessage)
+          .should('be.visible')
+    }
+
+    loginComEmailInvalido(email, password) {
+        cy.get(el.inputEmail)
+          .type(email)
+          .should('have.prop', 'validity')
+          .and('include', { valid: false})
+        cy.get(el.inputPassword)
+          .type(password)
+        cy.get('button').contains('Entrar')
+          .should('be.visible')
+          .click()
+    }
+
+    validarRedirecionamentoDeslogado() {
+        cy.url()
+          .should('be.equal', `${Cypress.config("baseUrl")}/login`)
+    }
+
+    validarQueLogoutRedirecionaParaLogin() {
+        cy.get('span>.v-avatar')
+          .click()
+        cy.contains('Sair')
+          .should('be.visible')
+          .click()
+        cy.url()
+          .should('be.equal', `${Cypress.config("baseUrl")}/`)
+    }
+}
+
+export default new Login()
