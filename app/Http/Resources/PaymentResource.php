ff487c737fc9c2262e37e429eb1682ce0cdd46fd
Gabriel Moreira <gmdoas.dev@gmail.com>
2026-04-06 15:43:06 -0300

    adicionando resource

--- app/Http/Resources/PaymentResource.php
+++ app/Http/Resources/PaymentResource.php
@@ -9,11 +9,21 @@ class PaymentResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+
+            'project_id' => $this->project_id,
+
+            'creditor_requested_at' => $this->creditor_requested_at?->format('Y-m-d'),
+            'creditor_registration_number' => $this->creditor_registration_number,
+            'communication_sent_at' => $this->communication_sent_at?->format('Y-m-d'),
+            'contact_notes' => $this->contact_notes,
+
+            'created_at' => $this->created_at?->toISOString(),
+            'updated_at' => $this->updated_at?->toISOString(),
+        ];
     }
 }
