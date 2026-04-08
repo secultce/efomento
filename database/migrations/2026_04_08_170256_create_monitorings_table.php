ed33adbb9c996b2feffe6e68621e7c9c0e4ad8bc
Jefferson Oliveira <jeff.doliveira1@outlook.com>
2026-04-08 16:44:35 -0300

    feat: Model de Monitoramento

---
+++ database/migrations/2026_04_08_170256_create_monitorings_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('monitorings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id')->constrained();
+            $table->foreignId('created_by')->constrained('users');
+
+            $table->date('effective_date_of_the_instrument'); // Data de início de vigência do instrumento
+            $table->date('expiration_date_of_the_instrument'); // Data de término de vigência do instrumento
+            $table->date('deadline_for_completing_report'); // Prazo de preenchimento de relatório de monitoramento
+            $table->date('deadline_for_analysis_and_issuance_of_the_opinion'); // Prazo para análise e emissão do parecer de monitoramento
+            $table->date('date_of_processing_of_the_opinion_via_suite'); // Data da tramitação do parecer via Suite
+            $table->date('date_of_notification_to_the_agent'); // Data da notificação ao agente
+            $table->text('observations')->nullable();
+
+            $table->timestamp('processed_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('monitorings');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             LegalAnalysisSeeder::class,
             BudgetSeeder::class,
             PaymentSeeder::class,
+            MonitoringSeeder::class,
         ]);
     }
 }
