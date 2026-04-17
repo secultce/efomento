2f6618386c682986ac5df3b5fee27f4b5f19a5a7
Gabriel Moreira <gmdoas.dev@gmail.com>
2026-04-17 14:45:35 -0300

    adicionando import do has many

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -13,6 +13,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Builder;
 use OwenIt\Auditing\Auditable as AuditableTrait;
