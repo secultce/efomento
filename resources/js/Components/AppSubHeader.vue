420dc6c3e9599e05b3f7f160338cd5f9914b26a0
Junior-Shyko <franciscoanto@gmail.com>
2026-04-27 12:30:59 -0300

    :sparkles: #182 Add botão de voltar

--- resources/js/Components/AppSubHeader.vue
+++ resources/js/Components/AppSubHeader.vue
@@ -1,5 +1,48 @@
+<script setup>
+import { router } from '@inertiajs/vue3'
+
+const props = defineProps({
+    variant: {
+        type: String,
+        default: 'default',
+    },
+    backLabel: {
+        type: String,
+        default: 'Voltar',
+    },
+    backRoute: {
+        type: String,
+        default: null,
+    },
+    showBack: {
+        type: Boolean,
+        default: true,
+    },
+})
+
+function goBack() {
+    const referrer = document.referrer
+    const cameFromLogin = referrer !== '' && new URL(referrer).pathname === '/login'
+
+    if (!cameFromLogin && window.history.length > 1) {
+        window.history.back()
+    } else if (props.backRoute) {
+        router.visit(props.backRoute)
+    }
+}
+</script>
 <template>
-    <div class="bg-subheader px-6 py-4">
-        <slot />
+    <div :class="variant === 'large' ? 'bg-subheader !w-full py-4' : 'px-6 py-4 bg-subheader'">
+        <v-row>
+            <v-col cols="auto" :style="!showBack ? 'visibility: hidden' : ''">
+                <v-btn color="white" @click="goBack">
+                    <v-icon>ms:arrow_left_alt</v-icon>
+                    <span class="ml-2">{{ backLabel }}</span>
+                </v-btn>
+            </v-col>
+            <v-col>
+                <slot />
+            </v-col>
+        </v-row>
     </div>
 </template>
