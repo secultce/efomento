3cdb5e7467f8002b8805386057f1c3ccfa4e8dd6
Junior-Shyko <franciscoanto@gmail.com>
2026-05-04 09:18:57 -0300

    :poop: #186 removido arquivo de sujeira

--- database/migrations/2026_03_16_161002_create_projects_table.php
+++ database/migrations/2026_03_16_161002_create_projects_table.php
@@ -27,8 +27,6 @@ return new class extends Migration
             $table->jsonb('data_registration')->nullable();
             $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
             $table->index('created_by');
-            $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
-            $table->index('created_by');
             $table->string('title_project')->nullable();
 
             $table->timestamps();
