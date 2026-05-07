51c612d171022732af0967a7f9b14e7d0ef37a67
Gabriel Moreira <gmdoas.dev@gmail.com>
2026-05-07 12:15:39 -0300

    correção do aux links

--- resources/js/Components/AuxLinks.vue
+++ resources/js/Components/AuxLinks.vue
@@ -12,14 +12,14 @@ const { openExternal } = useExternalLink()
            </div>
         </div>
          <div class="flex items-center hover:border-gray-600 cursor-pointer hover:text-green-600">
-           <img src="/images/logos/suite.png" alt="logo mapas" class="w-20 h-6" />
+           <img src="/images/logos/suite.png" alt="logo suite" class="w-20 h-6" />
            <div>
              <p class="text-sm font-bold" @click="openExternal('https://suite.ce.gov.br/')">Suite</p>
              <p class="text-xs text-gray-600">Acesse o processo no suite</p>
            </div>
         </div>
          <div class="flex items-center gap-2 hover:border-gray-600 cursor-pointer hover:text-green-600">
-           <img src="/images/logos/eparc.png" alt="logo mapas" class="w-20 h-6" />
+           <img src="/images/logos/eparc.png" alt="logo eparc" class="w-20 h-6" />
            <div>
              <p class="text-sm font-bold" @click="openExternal('https://e-parcerias.cge.ce.gov.br/e-parcerias-web/padrao-web/paginas/seguranca/login.seam')">E-Parcerias</p>
              <p class="text-xs text-gray-600">Acesse o processo no E-Parcerias</p>
