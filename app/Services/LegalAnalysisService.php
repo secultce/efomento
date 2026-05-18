61523d057075174d2f825a8a050e2a577725c789
Junior-Shyko <franciscoanto@gmail.com>
2026-05-18 13:54:43 -0300

    :hammer: #23 Refatorando para firstOrCreate()

--- app/Services/LegalAnalysisService.php
+++ app/Services/LegalAnalysisService.php
@@ -4,16 +4,13 @@ namespace App\Services;
 
 use App\Enums\FileStatus;
 use App\Models\File;
-use App\Models\LegalAnalysis;
 use App\Models\Project;
 
 class LegalAnalysisService
 {
     public function updateFileStatus(Project $project, File $file, FileStatus $status): void
     {
-        $analysis = $project->legalAnalysis ?? LegalAnalysis::create([
-            'project_id' => $project->id,
-        ]);
+        $analysis = $project->legalAnalysis()->firstOrCreate([]);
 
         $analysis->files()->updateOrCreate(
             ['file_id' => $file->id],
