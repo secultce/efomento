9ba98979984a0ec0583e22a96065f77917ae69af
Gabriel Moreira <gmdoas.dev@gmail.com>
2026-05-19 12:55:50 -0300

    adicionando notificação

--- app/Services/ProjectStageService.php
+++ app/Services/ProjectStageService.php
@@ -5,11 +5,16 @@ namespace App\Services;
 use App\Enums\ProjectStageStatus;
 use App\Models\ProjectStage;
 use App\Models\User;
+use App\Support\Notify;
 use Illuminate\Auth\Access\AuthorizationException;
 use InvalidArgumentException;
 
 class ProjectStageService
 {
+    public function __construct(
+        private Notify $notify
+    ) {}
+
     public function advance(ProjectStage $stage, User $user): void
     {
         if (! $user->hasAnyRole($stage->responsible_sector)) {
@@ -29,15 +34,57 @@ class ProjectStageService
                 'Não é possível avançar: existem etapas anteriores não aprovadas.'
             );
         }
+        $previousStage = $stage;
 
         $stage->markApproved();
 
         $next = $stage->getNextStage();
+
         if ($next) {
             $next->update([
                 'status' => ProjectStageStatus::EM_ANDAMENTO,
                 'started_at' => now(),
             ]);
+
+            $previousName = str($previousStage->slug->value)
+                ->replace('_', ' ')
+                ->title();
+
+            $nextName = str($next->slug->value)
+                ->replace('_', ' ')
+                ->title();
+
+            $message = sprintf(
+                '%s tramitou o projeto %s de %s para %s.',
+                $user->name,
+                $stage->project->title_project,
+                $previousName,
+                $nextName
+            );
+
+            $title = sprintf(
+                'Projeto %s Atualizado',
+                $stage->project->title_project,
+            );
+
+            $this->notify
+                ->allUsers()
+                ->info(
+                    $message,
+                    $title,
+                    (object) [
+                        'route' => 'notices.projects.show',
+                        'params' => [
+                            'notice' => $stage->project->notice_id,
+                            'project' => $stage->project->id,
+
+                        ],
+                        'user' => [
+                            'name' => $user->name,
+                            'avatar' => $user->profile_picture,
+                        ],
+                    ]
+                );
         }
     }
 
