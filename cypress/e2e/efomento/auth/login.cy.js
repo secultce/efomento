379c0b11eeb57a398c6ec41ce5139c379a2d0737
jgaguiarm <jaimgaguiar@gmail.com>
2026-05-20 15:46:59 -0300

    Implementando correções nos testes para a funcionalidade de Login

--- cypress/e2e/efomento/auth/login.cy.js
+++ cypress/e2e/efomento/auth/login.cy.js
@@ -2,37 +2,34 @@ import Login from '../../../pages/auth';
 import Notice from '../../../pages/notice';
 
 describe('Login', () => {
-    let data;
+    beforeEach(() => {
+        cy.fixture('users').as('user');
+        Login.accessLoginPage();
+    });
 
-    before(() => {
-        cy.fixture('users').then((tData) => {
-            data = tData;
+    describe('Positive Tests', () => {
+        it('Validate it is possible to login with valid credentials', function () {
+            Login.successLogin(this.user.valid_email, this.user.password, this.user.name);
+        });
+
+        it('Validate that accessing notice page when logged out redirects to login page', () => {
+            Notice.acessarPaginaDeEditais();
+            Login.validateUnloggedUserRedirectsToLogin();
+        });
+
+        it('Validate click on logout button redirects to login page', function () {
+            Login.successLogin(this.user.valid_email, this.user.password, this.user.name);
+            Login.validateLogoutRedirectsToLoginPage();
         });
     });
 
-    it('Validate it is possible to login with valid credentials', () => {
-        Login.accessLoginPage();
-        Login.successLogin(data.valid_email, data.password, data.name);
-    });
+    describe('Negative Tests', () => {
+        it('Validate it is not possible to login with invalid password', function () {
+            Login.loginWithInvalidPassword(this.user.valid_email, this.user.incorrect_password);
+        });
 
-    it('Validate it is not possible to login with invalid password', () => {
-        Login.accessLoginPage();
-        Login.loginWithInvalidPassword(data.valid_email, data.incorrect_password);
-    });
-
-    it('Validate it is possible to login with invalid email', () => {
-        Login.accessLoginPage();
-        Login.loginWithInvalidEmail(data.invalid_email, data.password);
-    });
-
-    it('Validate that accessing notice page when logged out redirects to login page', () => {
-        Notice.acessarPaginaDeEditais();
-        Login.validateUnlogedUserRedirectsToLogin();
-    });
-
-    it('Validate click on logout button redirects to login page', () => {
-        Login.accessLoginPage();
-        Login.successLogin(data.valid_email, data.password, data.name);
-        Login.validateLogoutRedirectsToLoginPage();
+        it('Validate it is not possible to login with invalid email', function () {
+            Login.loginWithInvalidEmail(this.user.invalid_email, this.user.password);
+        });
     });
 });
