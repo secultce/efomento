d662d6b96b084946dd2c4fd7658c7cb03a9484ff
Junior-Shyko <franciscoanto@gmail.com>
2026-05-21 16:00:09 -0300

    :hammer: #198 Adicionado tipo de fiscal em massa.

--- app/Services/ProjectSupervisorService.php
+++ app/Services/ProjectSupervisorService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\OpeningSupervisor;
 use App\Models\Project;
 use Illuminate\Support\Facades\DB;
 
@@ -9,17 +10,24 @@ class ProjectSupervisorService
 {
     public function assign(array $projectIds, array $supervisorIds): void
     {
+        $types = [OpeningSupervisor::TYPE_PRINCIPAL, OpeningSupervisor::TYPE_ALTERNATE];
+
+        $supervisors = collect($supervisorIds)
+            ->values()
+            ->map(fn ($id, $index) => ['id' => $id, 'type' => $types[$index] ?? null])
+            ->toArray();
+
         $projects = Project::with('opening')
             ->whereIn('id', $projectIds)
             ->get();
 
-        DB::transaction(function () use ($projects, $supervisorIds) {
+        DB::transaction(function () use ($projects, $supervisors) {
             foreach ($projects as $project) {
                 if (! $project->opening) {
                     throw new \Exception("Projeto {$project->id} não possui abertura.");
                 }
 
-                $project->opening->assignSupervisors($supervisorIds);
+                $project->opening->assignSupervisors($supervisors);
             }
         });
     }
