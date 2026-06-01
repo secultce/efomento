364c9798d5d28f9af2b8fb3aa67ca93d7ded7078
Gabriel Moreira <gmdoas.dev@gmail.com>
2026-06-01 17:18:53 -0300

    adicionando abreviação

--- resources/js/Schemas/Config/documentConfig.js
+++ resources/js/Schemas/Config/documentConfig.js
@@ -22,15 +22,15 @@ export const documentConfigs = {
 
     [DOCUMENT_TYPES.ET]: {
         name: 'Extrato do Termo',
-        titleCreate: 'Criar extrato do termo',
-        titleEdit: 'Editar extrato do termo',
+        titleCreate: 'Criar extrato do termo (ET)',
+        titleEdit: 'Editar extrato do termo (ET)',
         save: 'et',
     },
 
     [DOCUMENT_TYPES.PJ]: {
         name: 'Parecer Jurídico',
-        titleCreate: 'Criar parecer jurídico',
-        titleEdit: 'Editar parecer jurídico',
+        titleCreate: 'Criar parecer jurídico (PJ)',
+        titleEdit: 'Editar parecer jurídico (PJ)',
         save: 'pj',
     },
 };
