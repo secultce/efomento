556a3aa1b0cbb6888d9e0939adbda5782ac27636
Junior-Shyko <franciscoanto@gmail.com>
2026-07-01 14:40:27 -0300

    :construction: #183 add a funcao na lista de usuarios

--- resources/js/Pages/Groups/Users.vue
+++ resources/js/Pages/Groups/Users.vue
@@ -6,8 +6,8 @@ defineProps({
 
 <template>
     <div v-for="user in users" :key="user.id">
-        <v-sheet class="pa-2" border height="40">
-            <v-row>
+        <v-sheet class="pa-2" border>
+            <v-row align="center" no-gutters>
                 <v-col cols="1">
                     <v-checkbox-btn></v-checkbox-btn>
                 </v-col>
@@ -19,15 +19,28 @@ defineProps({
                         ></v-img>
                     </v-avatar>
                 </v-col>
-                <v-col cols="8">
-                    <h4 class="font-weight-bold">{{ user.id }} - {{ user.name }}</h4>
+                <v-col cols="4">
+                    <h4 class="font-weight-bold">{{ user.name }}</h4>
                 </v-col>
-                <v-col cols="2">
+                <v-col cols="4">
+                    <v-chip v-if="user.roles.length === 0" size="small" color="grey" variant="tonal" class="mr-1">
+                        sem atribuição
+                    </v-chip>
+                    <v-chip
+                        v-for="role in user.roles"
+                        :key="role.name"
+                        size="small"
+                        color="primary"
+                        variant="tonal"
+                        class="mr-1"
+                    >
+                        {{ role.label }}
+                    </v-chip>
+                </v-col>
+                <v-col cols="2" class="d-flex justify-end">
                     <v-btn variant="outlined" color="primary" rounded="lg" height="25">atribuir função</v-btn>
                 </v-col>
             </v-row>
         </v-sheet>
     </div>
 </template>
-
-<style scoped></style>
