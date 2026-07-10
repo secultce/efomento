4bfc2619c751e2cbfd252178a69052bef9431b70
Gabriel Moreira <gmdoas.dev@gmail.com>
2026-07-10 15:21:21 -0300

    ajustando o botão de voltar

--- resources/js/Components/AppSubHeader.vue
+++ resources/js/Components/AppSubHeader.vue
@@ -21,41 +21,53 @@ const props = defineProps({
 });
 
 function goBack() {
-    const referrer = document.referrer;
-    const cameFromLogin = referrer !== '' && new URL(referrer).pathname === '/login';
-
-    if (!cameFromLogin && window.history.length > 1) {
+    if (!props.backRoute) {
         window.history.back();
-        setTimeout(() => {
-            router.reload({
-                preserveState: false,
-                preserveScroll: true,
-            });
-        }, 50);
-
         return;
     }
 
-    if (props.backRoute) {
-        router.visit(props.backRoute, {
+    try {
+        const targetUrl = new URL(props.backRoute, window.location.origin);
+
+        if (targetUrl.origin !== window.location.origin) {
+            window.location.assign(targetUrl.href);
+            return;
+        }
+
+        const relativeUrl = `${targetUrl.pathname}${targetUrl.search}${targetUrl.hash}`;
+
+        router.visit(relativeUrl, {
+            method: 'get',
             preserveState: false,
+            preserveScroll: true,
+            replace: true,
         });
+    } catch (error) {
+        console.error('Rota de retorno inválida:', props.backRoute, error);
+
+        window.location.assign(props.backRoute);
     }
 }
 </script>
+
 <template>
-    <div :class="variant === 'large' ? 'bg-subheader !w-full py-4' : 'px-6 py-4 bg-subheader'">
-        <v-row v-if="showBack" :style="showBack ? '' : 'visibility: hidden'">
-            <v-col cols="auto">
-                <v-btn color="white" data-cy="btnVoltar" @click="goBack">
+    <div :class="variant === 'large' ? 'bg-subheader w-full py-4' : 'bg-subheader px-6 py-4'">
+        <v-row v-if="showBack" align="center" no-gutters>
+            <v-col cols="auto" class="mr-4">
+                <v-btn type="button" color="white" data-cy="btnVoltar" @click="goBack">
                     <v-icon>ms:arrow_left_alt</v-icon>
-                    <span class="ml-2">{{ backLabel }}</span>
+
+                    <span class="ml-2">
+                        {{ backLabel }}
+                    </span>
                 </v-btn>
             </v-col>
+
             <v-col>
                 <slot />
             </v-col>
         </v-row>
+
         <slot v-else />
     </div>
 </template>
