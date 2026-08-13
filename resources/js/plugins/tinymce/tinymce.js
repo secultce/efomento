86f8488244c796bcf482207013e634d9ed677f1a
Gabriel Moreira <gmdoas.dev@gmail.com>
2026-08-13 14:32:28 -0300

    ajustes tinymce setup

--- resources/js/plugins/tinymce/tinymce.js
+++ resources/js/plugins/tinymce/tinymce.js
@@ -1,17 +1,33 @@
 import 'tinymce/tinymce';
 
-// Theme
+// Required TinyMCE components
+import 'tinymce/models/dom';
 import 'tinymce/themes/silver';
-
-// Icons
 import 'tinymce/icons/default';
 
 // Plugins
+import 'tinymce/plugins/autolink';
+import 'tinymce/plugins/code';
+import 'tinymce/plugins/directionality';
+import 'tinymce/plugins/fullscreen';
+import 'tinymce/plugins/help';
+import 'tinymce/plugins/help/js/i18n/keynav/pt_BR';
+import 'tinymce/plugins/image';
+import 'tinymce/plugins/insertdatetime';
 import 'tinymce/plugins/link';
 import 'tinymce/plugins/lists';
+import 'tinymce/plugins/media';
+import 'tinymce/plugins/preview';
+import 'tinymce/plugins/searchreplace';
 import 'tinymce/plugins/table';
-import 'tinymce/plugins/code';
-import 'tinymce/plugins/image';
+import 'tinymce/plugins/visualblocks';
+import 'tinymce/plugins/visualchars';
 import 'tinymce/plugins/wordcount';
+
+// Language
 import '@/plugins/tinymce/langs/pt_BR';
-import 'tinymce/skins/ui/oxide/skin.css';
+
+// Self-hosted UI and content styles
+import 'tinymce/skins/ui/oxide/skin.js';
+import 'tinymce/skins/ui/oxide/content.js';
+import 'tinymce/skins/content/default/content.js';
