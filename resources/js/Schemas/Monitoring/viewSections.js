e1f5e0f115f9409de40f5412d7f42225c0c7aa4f
Gabriel Moreira <gmdoas.dev@gmail.com>
2026-08-27 11:40:56 -0300

    removendo fallback do frontend

--- resources/js/Schemas/Monitoring/viewSections.js
+++ resources/js/Schemas/Monitoring/viewSections.js
@@ -22,7 +22,7 @@ export const viewSections = [
                     if (!paymentDate || !signedAt) return null;
 
                     const daysBetweenSignedAndPayment = daysBetweenDates(signedAt, paymentDate);
-                    const requestDeadlineDays = Number(project.notice?.monitoring_report_request_deadline_days ?? 120);
+                    const requestDeadlineDays = Number(project.notice?.monitoring_report_request_deadline_days);
 
                     const date = new Date(paymentDate);
                     date.setDate(date.getDate() + requestDeadlineDays + daysBetweenSignedAndPayment);
