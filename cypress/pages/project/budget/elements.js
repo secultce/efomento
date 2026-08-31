4384d09d855388ce7b11958d4d782099ec0f5c61
jgaguiarm <jaimgaguiar@gmail.com>
2026-08-31 16:48:32 -0300

    Implementa alteração nos atributos data-cy na aba de Orçamento

--- cypress/pages/project/budget/elements.js
+++ cypress/pages/project/budget/elements.js
@@ -1,10 +1,10 @@
 export const elements = {
     budgetTab: '[data-cy=budget-tab]',
     budgeRightPanel: '[data-cy=budget-right-panel]',
-    processingDateCodip: '[data-cy=processing-date-codip-input]',
-    processingDateCoafi: '[data-cy=processing-date-coafi-input]',
-    noticeInstallmentNumberInput: '[data-cy=notice-installment-number-imput]',
-    installmentAmountInput: '[data-cy=installment-amount-input]',
-    installmentRequestDateInput: '[data-cy=installment-request-date-input]',
-    installmentObservationsInput: '[data-cy=installments-observations-input]',
+    processingDateCodip: '[data-cy=processing-date-codip-input] input',
+    processingDateCoafi: '[data-cy=processing-date-coafi-input] input',
+    noticeInstallmentNumberInput: '[data-cy=notice-installment-number-imput] input',
+    installmentAmountInput: '[data-cy=installment-amount-input] input',
+    installmentRequestDateInput: '[data-cy=installment-request-date-input] input',
+    installmentObservationsInput: '[data-cy=installments-observations-input] textarea',
 };
