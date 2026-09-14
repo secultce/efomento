838067c68150204a86f723051d549249345f19ef
Gabriel Moreira <gmdoas.dev@gmail.com>
2026-09-14 12:09:14 -0300

    coderrabit fix

--- app/Http/Controllers/TrustedDeviceController.php
+++ app/Http/Controllers/TrustedDeviceController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TrustedDevice;
 use App\Services\TrustedDeviceService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class TrustedDeviceController extends Controller
 {
-    public function destroy(Request $request, int $device, TrustedDeviceService $devices): RedirectResponse
+    public function destroy(Request $request, TrustedDevice $device, TrustedDeviceService $devices): RedirectResponse
     {
-        $devices->revokeDevice($request, $device);
+        $devices->revokeDevice($request, $device->id);
 
         return redirect()->route('profile.edit');
     }
